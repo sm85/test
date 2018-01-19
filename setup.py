27b5915a0edc5f21c29176cbfcfa9c7fb3a5bf32
sm85 <31991964+sm85@users.noreply.github.com>
2018-01-18 19:47:28 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'GT',
+  packages = ['GT'], # this must be the same as the name above
+  version = '0.1',
+  description = 'puma_test',
+  author = 'smishra',
+  author_email = 'salinimishra@gatech.edu',
+  url = 'https://github.gatech.edu/smishra85/puma/GT', # use the URL to the github repo
+  download_url = 'https://github.gatech.edu/smishra85/puma/GT/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
---
+++ test.py
@@ -0,0 +1,8 @@
+from GT import cs
+from GT import civil
+
+computerscience=cs()
+computerscience.printMembers()
+
+civilengineering=civil()
+civilengineering.printMembers()
